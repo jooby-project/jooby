bbf0b1e594d0bd2212a80aeb243b6bd990eadc0d
Edgar Espina <espina.edgar@gmail.com>
2015-04-23 14:22:48 -0300

    remove mysql test from jdbi and use h2.mem db

--- coverage-report/src/test/java/org/jooby/jdbi/JdbiInClauseFeature.java
+++ coverage-report/src/test/java/org/jooby/jdbi/JdbiInClauseFeature.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.jooby.test.ServerFeature;
 import org.junit.Test;
 import org.skife.jdbi.v2.Handle;
-import org.skife.jdbi.v2.logging.FormattedLog;
 import org.skife.jdbi.v2.util.StringMapper;
 
 import com.typesafe.config.ConfigFactory;
@@ -16,28 +15,12 @@ public class JdbiInClauseFeature extends ServerFeature {
   {
 
     use(ConfigFactory.empty()
-        .withValue("db.url", ConfigValueFactory.fromAnyRef("jdbc:mysql://localhost/test"))
-        .withValue("db.user", ConfigValueFactory.fromAnyRef("root")));
+        .withValue("db", ConfigValueFactory.fromAnyRef("mem")));
 
-    use(new Jdbi().doWith((dbi, conf) -> {
-      dbi.setSQLLog(new FormattedLog() {
-
-        @Override
-        protected void log(final String msg) {
-          System.out.println(msg);
-        }
-
-        @Override
-        protected boolean isEnabled() {
-          return true;
-        }
-      });
-    }));
+    use(new Jdbi());
 
     get("/named", req -> {
       try (Handle h = req.require(Handle.class)) {
-        h.execute("drop table members");
-
         h.execute("create table members (id int primary key, name varchar(100))");
 
         h.execute("insert into members (id, name) values (?, ?)", 1, "Jooby");
@@ -54,13 +37,6 @@ public class JdbiInClauseFeature extends ServerFeature {
 
     get("/pos", req -> {
       try (Handle h = req.require(Handle.class)) {
-        h.execute("drop table members");
-
-        h.execute("create table members (id int primary key, name varchar(100))");
-
-        h.execute("insert into members (id, name) values (?, ?)", 1, "Jooby");
-        h.execute("insert into members (id, name) values (?, ?)", 2, "Rock");
-
         List<String> name = h.createQuery("select name from members where id in (?)")
             .bind(0, req.param("id").toList(Integer.class).toArray(new Integer[0]))
             .map(StringMapper.FIRST)
@@ -76,12 +52,10 @@ public class JdbiInClauseFeature extends ServerFeature {
     request()
         .get("/named?id=1&id=2")
         .expect("[Jooby, Rock]");
-  }
 
-  @Test
-  public void pos() throws Exception {
     request()
         .get("/pos?id=1&id=2")
         .expect("[Jooby, Rock]");
   }
+
 }
