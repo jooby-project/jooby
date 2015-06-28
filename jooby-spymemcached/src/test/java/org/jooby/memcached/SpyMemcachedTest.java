9db3550c71e7d35b8cb5592fb74b07172dfd528f
Edgar Espina <espina.edgar@gmail.com>
2015-06-28 20:12:40 -0300

    unit test and code coverage for memcached module + session store ref #91

--- jooby-spymemcached/src/test/java/org/jooby/memcached/SpyMemcachedTest.java
+++ jooby-spymemcached/src/test/java/org/jooby/memcached/SpyMemcachedTest.java
@@ -11,6 +11,7 @@ import net.spy.memcached.ConnectionFactoryBuilder.Protocol;
 import net.spy.memcached.FailureMode;
 import net.spy.memcached.MemcachedClient;
 import net.spy.memcached.auth.AuthDescriptor;
+import net.spy.memcached.metrics.MetricType;
 
 import org.jooby.Env;
 import org.jooby.internal.memcached.MemcachedClientProvider;
@@ -36,7 +37,7 @@ public class SpyMemcachedTest {
     unit.registerMock(ConnectionFactoryBuilder.class, cfb);
   };
 
-  private Block defsProperties = unit -> {
+  private Block defprops = unit -> {
     ConnectionFactoryBuilder cfb = unit.get(ConnectionFactoryBuilder.class);
     expect(cfb.setAuthWaitTime(1000L)).andReturn(cfb);
     expect(cfb.setDaemon(false)).andReturn(cfb);
@@ -44,6 +45,24 @@ public class SpyMemcachedTest {
     expect(cfb.setLocatorType(Locator.ARRAY_MOD)).andReturn(cfb);
     expect(cfb.setMaxReconnectDelay(30)).andReturn(cfb);
     expect(cfb.setProtocol(Protocol.TEXT)).andReturn(cfb);
+  };
+
+  private Block fullprops = unit -> {
+    ConnectionFactoryBuilder cfb = unit.get(ConnectionFactoryBuilder.class);
+    expect(cfb.setAuthWaitTime(1000L)).andReturn(cfb);
+    expect(cfb.setDaemon(false)).andReturn(cfb);
+    expect(cfb.setFailureMode(FailureMode.Redistribute)).andReturn(cfb);
+    expect(cfb.setLocatorType(Locator.ARRAY_MOD)).andReturn(cfb);
+    expect(cfb.setMaxReconnectDelay(30)).andReturn(cfb);
+    expect(cfb.setProtocol(Protocol.TEXT)).andReturn(cfb);
+
+    expect(cfb.setEnableMetrics(MetricType.OFF)).andReturn(cfb);
+    expect(cfb.setOpQueueMaxBlockTime(1000L)).andReturn(cfb);
+    expect(cfb.setOpTimeout(1000L)).andReturn(cfb);
+    expect(cfb.setReadBufferSize(100)).andReturn(cfb);
+    expect(cfb.setShouldOptimize(true)).andReturn(cfb);
+    expect(cfb.setTimeoutExceptionThreshold(10)).andReturn(cfb);
+    expect(cfb.setUseNagleAlgorithm(true)).andReturn(cfb);
   };
 
   @SuppressWarnings("unchecked")
@@ -64,7 +83,45 @@ public class SpyMemcachedTest {
         .withValue("memcached.server", ConfigValueFactory.fromAnyRef("localhost:11211"));
     new MockUnit(Env.class, Binder.class)
         .expect(cfb)
-        .expect(defsProperties)
+        .expect(defprops)
+        .expect(bind)
+        .run(unit -> {
+          new SpyMemcached()
+              .configure(unit.get(Env.class), config, unit.get(Binder.class));
+        });
+  }
+
+  @Test
+  public void fullprops() throws Exception {
+    Config config = new SpyMemcached().config()
+        .withValue("memcached.server", ConfigValueFactory.fromAnyRef("localhost:11211"))
+        .withValue("memcached.enableMetrics", ConfigValueFactory.fromAnyRef("off"))
+        .withValue("memcached.opQueueMaxBlockTime", ConfigValueFactory.fromAnyRef("1s"))
+        .withValue("memcached.opTimeout", ConfigValueFactory.fromAnyRef("1s"))
+        .withValue("memcached.readBufferSize", ConfigValueFactory.fromAnyRef(100))
+        .withValue("memcached.shouldOptimize", ConfigValueFactory.fromAnyRef(true))
+        .withValue("memcached.timeoutExceptionThreshold", ConfigValueFactory.fromAnyRef(10))
+        .withValue("memcached.useNagleAlgorithm", ConfigValueFactory.fromAnyRef(true));
+
+    new MockUnit(Env.class, Binder.class)
+        .expect(cfb)
+        .expect(fullprops)
+        .expect(bind)
+        .run(unit -> {
+          new SpyMemcached()
+              .configure(unit.get(Env.class), config, unit.get(Binder.class));
+        });
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void enumErr() throws Exception {
+    Config config = new SpyMemcached().config()
+        .withValue("memcached.server", ConfigValueFactory.fromAnyRef("localhost:11211"))
+        .withValue("memcached.enableMetrics", ConfigValueFactory.fromAnyRef("invalid"));
+
+    new MockUnit(Env.class, Binder.class)
+        .expect(cfb)
+        .expect(defprops)
         .expect(bind)
         .run(unit -> {
           new SpyMemcached()
@@ -79,7 +136,7 @@ public class SpyMemcachedTest {
             ConfigValueFactory.fromAnyRef(Arrays.asList("localhost:11211", "localhost:11212")));
     new MockUnit(Env.class, Binder.class)
         .expect(cfb)
-        .expect(defsProperties)
+        .expect(defprops)
         .expect(bind)
         .run(unit -> {
           new SpyMemcached()
@@ -94,7 +151,7 @@ public class SpyMemcachedTest {
             ConfigValueFactory.fromAnyRef(Arrays.asList("localhost:11211", "localhost:11212")));
     new MockUnit(Env.class, Binder.class, AuthDescriptor.class)
         .expect(cfb)
-        .expect(defsProperties)
+        .expect(defprops)
         .expect(unit -> {
           ConnectionFactoryBuilder cfb = unit.get(ConnectionFactoryBuilder.class);
           expect(cfb.setAuthDescriptor(unit.get(AuthDescriptor.class))).andReturn(cfb);
