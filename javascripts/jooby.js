e532dff9ac139d4080eb7de7b2891aa049e44a2c
Edgar Espina <espina.edgar@gmail.com>
2015-07-13 12:24:58 -0300

    prepare for next release

--- javascripts/jooby.js
+++ javascripts/jooby.js
@@ -8,18 +8,35 @@
    * Find the first .highlight element and prepend a toolbar with a copy to clipboard button.
    */
   var copyToClipboard = function () {
-    var $button = $('<span class="copy-button octicon octicon-clippy" title="copy to clipboard"></span>');
-    var $div = $('<div class="copy-bar"></div>')
-    $('.highlight').prepend($div.append($button));
 
-    var client = new ZeroClipboard($button);
+    $('.highlight').each(function () {
+      var $button = $('<span class="copy-button octicon octicon-clippy" title="copy to clipboard"></span>');
+      var $div = $('<div class="copy-bar"></div>');
+      var $el = $(this);
 
-    client.on( 'ready', function(event) {
+      $el.prepend($div.append($button));
 
-      client.on( 'copy', function(event) {
-        event.clipboardData.setData('text/plain', $button.parent().parent().find('pre code').text());
+      var client = new ZeroClipboard($button);
+
+      client.on( 'ready', function(event) {
+        client.on( 'copy', function(event) {
+          event.clipboardData.setData('text/plain', $el.find('pre code').text());
+        });
       });
 
+    });
+  };
+
+  var lang = function () {
+    $('ul.nav-lang li').click(function () {
+      var $li = $(this),
+          lang = $li.data('lang')
+          $ullang = $('ul.lang');
+      $li.parent().find('li').removeClass('active');
+      $li.addClass('active');
+
+      $ullang.find('li').removeClass('active');
+      $ullang.find('li.' + lang).addClass('active');
     });
   };
 
@@ -34,6 +51,8 @@
   $(function () {
     // page
     page();
+    // lang
+    lang();
     // clipboard
     if (window.ZeroClipboard) {
       copyToClipboard();
