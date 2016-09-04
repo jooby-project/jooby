43b64ff0c3778592ec2451eb85300b131a537553
Edgar Espina <espina.edgar@gmail.com>
2016-09-04 19:21:51 -0300

    Build fix for linux + license headers

--- jooby-assets-jsass/src/test/java/org/jooby/assets/JSassTest.java
+++ jooby-assets-jsass/src/test/java/org/jooby/assets/JSassTest.java
@@ -167,24 +167,24 @@ public class JSassTest {
 
   @Test
   public void inlineSourceMap() throws Exception {
-    assertEquals(".foo {\n" +
+    String output = new Sass().set("sourcemap", "inline")
+        .process("/styles.scss",
+            "@import 'foo.scss';\n$font-stack:    Helvetica, sans-serif;\n" +
+                "$primary-color: #333;\n" +
+                "\n" +
+                "body {\n" +
+                "  font: 100% $font-stack;\n" +
+                "  color: $primary-color;\n" +
+                "}\n",
+            ConfigFactory.empty());
+    assertTrue(output.startsWith(".foo {\n" +
         "  color: #fff; }\n" +
         "\n" +
         "body {\n" +
         "  font: 100% Helvetica, sans-serif;\n" +
         "  color: #333; }\n" +
         "\n" +
-        "/*# sourceMappingURL=data:application/json;base64,ewoJInZlcnNpb24iOiAzLAoJImZpbGUiOiAiLi4vLi4vLi4vLi4vLi4vc3R5bGVzLmNzcyIsCgkic291cmNlcyI6IFsKCQkiLi4vLi4vLi4vLi4vLi4vc3R5bGVzLnNjc3MiLAoJCSIuLi8uLi8uLi8uLi8uLi9zdHlsZXMuc2Nzcy9KU0FTU19DVVNUT00uc2NzcyIsCgkJInRhcmdldC90ZXN0LWNsYXNzZXMvZm9vLnNjc3MvSlNBU1NfUFJFX0lNUE9SVC5zY3NzIiwKCQkidGFyZ2V0L3Rlc3QtY2xhc3Nlcy9mb28uc2NzcyIsCgkJInRhcmdldC90ZXN0LWNsYXNzZXMvZm9vLnNjc3MvSlNBU1NfUE9TVF9JTVBPUlQuc2NzcyIKCV0sCgkibWFwcGluZ3MiOiAiQUdBQSxBQUFBLElBQUksQ0FBQztFQUNILEtBQUssRUFBRSxJQUFLLEdBQ2I7O0FIRUQsQUFBQSxJQUFJLENBQUM7RUFDSCxJQUFJLEVBQUUsSUFBSSxDQUpJLFNBQVMsRUFBRSxVQUFVO0VBS25DLEtBQUssRUFKUyxJQUFJLEdBS25CIiwKCSJuYW1lcyI6IFtdCn0= */",
-        new Sass().set("sourcemap", "inline")
-            .process("/styles.scss",
-                "@import 'foo.scss';\n$font-stack:    Helvetica, sans-serif;\n" +
-                    "$primary-color: #333;\n" +
-                    "\n" +
-                    "body {\n" +
-                    "  font: 100% $font-stack;\n" +
-                    "  color: $primary-color;\n" +
-                    "}\n",
-                ConfigFactory.empty()));
+        "/*# sourceMappingURL=data:application/json;base64,ewoJInZlcnNpb24iOiAzLAoJImZpbGUiOiAiLi4vLi4v"));
   }
 
   @Test
