660ceb5c7124fedb7c0336bf9e14b6c04d973010
Johannes Schneider <js@cedarsoft.com>
2016-11-03 21:20:58 +0100

    add annotated parameters to test

--- coverage-report/src/test/java/org/jooby/issues/CatResource.java
+++ coverage-report/src/test/java/org/jooby/issues/CatResource.java
@@ -7,6 +7,7 @@ import org.jooby.Session;
 import org.jooby.issues.i378.Cat;
 import org.jooby.mvc.Flash;
 import org.jooby.mvc.GET;
+import org.jooby.mvc.Local;
 import org.jooby.mvc.Path;
 import org.jooby.mvc.Produces;
 
@@ -27,7 +28,7 @@ public class CatResource {
   @Path("/api/cat/:name")
   @Produces("application/json")
   @GET
-  public Cat get(Request req, Response response, Session session, Route route, String name) {
+  public Cat get(Request req, Response response, Session session, Route route, String name, @Flash int flashParam, @Local double localParam) {
     Cat cat = new Cat();
     cat.setName(name);
 
