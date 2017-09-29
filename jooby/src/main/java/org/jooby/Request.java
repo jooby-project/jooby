2c699f15f34666453a943bbc169bbcf5b58f0645
Niklas Lochschmidt <nlochschmidt@users.noreply.github.com>
2017-09-29 18:32:30 +0200

    Change default behavior of Request.locale() (#878)
    Now uses `Locale#lookup` instead of `Locale#filter` + findFirst and
    fallback. As a result, it will return the locale with the closest match
    in case that is possible.
    
    Example: with a language setting of "en, de", a request with the
    `Accept-Language` header set to `de-CH` will now return `de` instead of
    `en`.

--- jooby/src/main/java/org/jooby/Request.java
+++ jooby/src/main/java/org/jooby/Request.java
@@ -1168,8 +1168,7 @@ public interface Request extends Registry {
    * @return A matching locale.
    */
   default Locale locale() {
-    return locale((ranges, locales) -> Locale.filter(ranges, locales).stream()
-        .findFirst()
+    return locale((priorityList, locales) -> Optional.ofNullable(Locale.lookup(priorityList, locales))
         .orElse(locales.get(0)));
   }
 
