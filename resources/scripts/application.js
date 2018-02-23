0d43b59962824ec0e55c4740729630b3db155d76
Edgar Espina <espina.edgar@gmail.com>
2018-02-23 20:59:24 -0300

    Fix some javadoc links

--- resources/scripts/application.js
+++ resources/scripts/application.js
@@ -187,11 +187,8 @@ $(function(){
       textArea.select();
 
       try {
-        var successful = document.execCommand('copy');
-        var msg = successful ? 'successful' : 'unsuccessful';
-        console.log('Copying text command was ' + msg);
+        document.execCommand('copy');
       } catch (err) {
-        console.log('Oops, unable to copy');
       }
 
       document.body.removeChild(textArea);
