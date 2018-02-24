a464ee25b29c2b00bb045b6986e40ab2415da01a
Yorick Holkamp <yorick@nextpulse.nl>
2018-02-24 11:27:24 +0100

    More description exception added to the @Local injection logic.
    Rather than a `java.util.NoSuchElementException: No value present` we will now throw an exception that looks like `org.jooby.Err: Server Error(500): Could not find required local 'myLocal', which was required on /mypath`, to help with debugging.

--- jooby/src/test/java/org/jooby/internal/mvc/RequestParamTest.java
+++ jooby/src/test/java/org/jooby/internal/mvc/RequestParamTest.java
@@ -1,14 +1,22 @@
 package org.jooby.internal.mvc;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import java.lang.reflect.Parameter;
+import org.jooby.Request;
+import org.jooby.Response;
+import org.jooby.Route;
+import org.jooby.mvc.Header;
+import org.jooby.mvc.Local;
+import org.jooby.test.MockUnit;
+import org.junit.Test;
 
 import javax.inject.Named;
+import java.lang.reflect.Parameter;
+import java.util.Optional;
 
-import org.jooby.mvc.Header;
-import org.junit.Test;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class RequestParamTest {
 
@@ -30,7 +38,11 @@ public class RequestParamTest {
 
   public void eheader(@Header final String s) {
   }
-
+  
+  public void local(@Local String myLocal) {
+    
+  }
+  
   @Test
   public void name() throws Exception {
     assertEquals("javax", RequestParam.nameFor(param("javax")));
@@ -48,7 +60,51 @@ public class RequestParamTest {
         || "s".equals(RequestParam.nameFor(param("eheader"))));
 
   }
-
+  
+  @Test
+  public void requestParam_mvcLocal_valuePresent() throws Throwable {
+    Parameter param = param("local");
+    RequestParam requestParam = new RequestParam(param, "myLocal", param.getParameterizedType());
+    
+    // verify that with a mock request we can indeed retrieve the 'myLocal' value
+    new MockUnit(RequestParam.class, Request.class, Response.class, Route.Chain.class)
+        .expect(unit -> {
+          Request request = unit.get(Request.class);
+          expect(request.ifGet("myLocal")).andReturn(Optional.of("myCustomValue"));
+          verify();
+        })
+        .run((unit) -> {
+          Object output = requestParam.value(unit.get(Request.class), unit.get(Response.class), unit.get(Route.Chain.class));
+          assertEquals("myCustomValue", output);
+        });
+  }
+  
+  @Test
+  public void requestParam_mvcLocal_valueAbsent() throws Throwable {
+    Parameter param = param("local");
+    RequestParam requestParam = new RequestParam(param, "myLocal", param.getParameterizedType());
+    
+    // verify that we return a descriptive error when myLocal could not be located
+    new MockUnit(RequestParam.class, Request.class, Response.class, Route.Chain.class)
+        .expect(unit -> {
+          Request request = unit.get(Request.class);
+          expect(request.path()).andReturn("/mypath");
+          expect(request.ifGet("myLocal")).andReturn(Optional.empty());
+          verify();
+        })
+        .run((unit) -> {
+          RuntimeException exception = null;
+          try {
+            requestParam.value(unit.get(Request.class), unit.get(Response.class), unit.get(Route.Chain.class));
+          } catch(RuntimeException e) {
+            exception = e;
+            e.printStackTrace();
+          }
+          assertNotNull("Should have thrown an exception because the myLocal is not present", exception);
+          assertEquals("Server Error(500): Could not find required local 'myLocal', which was required on /mypath", exception.getMessage());
+        });
+  }
+  
   private Parameter param(final String name) throws Exception {
     return RequestParamTest.class.getDeclaredMethod(name, String.class).getParameters()[0];
   }
