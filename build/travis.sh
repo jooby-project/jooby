29b5afe1742335fb7c397944e82473c341fe855b
Edgar Espina <espina.edgar@gmail.com>
2018-07-09 21:27:47 -0300

    try to fix sonar push

--- build/travis.sh
+++ build/travis.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn -q -Dlogback.configurationFile=logback-travis.xml -Dcoverage.port=random -Dcoverage.securePort=random clean package coveralls:report -P coverage
+mvn -q -Dlogback.configurationFile=../../build/logback-travis.xml -Dcoverage.port=random -Dcoverage.securePort=random -Dsonar.organization=jooby -Dsonar.host.url=https://sonarcloud.io -Dsonar.branch.name=master clean install coveralls:report -P coverage
