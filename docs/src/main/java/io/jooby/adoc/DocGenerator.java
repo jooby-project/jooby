73c5a3525a7287ca2190c4478fc8253d7c3fe33d
Edgar Espina <espina.edgar@gmail.com>
2019-04-02 16:19:08 -0300

    remove dynamic dom manipulation with static

--- docs/src/main/java/io/jooby/adoc/DocGenerator.java
+++ docs/src/main/java/io/jooby/adoc/DocGenerator.java
@@ -21,12 +21,20 @@ import org.asciidoctor.Attributes;
 import org.asciidoctor.Options;
 import org.asciidoctor.Placement;
 import org.asciidoctor.SafeMode;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DocGenerator {
   public static final String VERSION = "2.0.0.M1";
@@ -66,7 +74,6 @@ public class DocGenerator {
     attributes.setAttribute("highlightjs-theme", "agate");
     attributes.setAttribute("favicon", "images/favicon96.png");
 
-
     Path outdir = asciidoc.resolve("site");
     if (!Files.exists(outdir)) {
       Files.createDirectories(outdir);
@@ -93,7 +100,13 @@ public class DocGenerator {
         // js
         basedir.resolve("js")
     );
+
     asciidoctor.convertFile(asciidoc.resolve("index.adoc").toFile(), options);
+
+    Path index = outdir.resolve("index.html");
+
+    // adjust toc
+    Files.write(index, document(index).getBytes(StandardCharsets.UTF_8));
 
     if (publish) {
       Path website = basedir.resolve("target")// Paths.get(System.getProperty("java.io.tmpdir"))
@@ -109,6 +122,92 @@ public class DocGenerator {
 
       FileUtils.copyDirectory(outdir.toFile(), website.toFile());
       git.commit("Sync documentation");
+    }
+  }
+
+  private static String document(Path index) throws IOException {
+    Document doc = Jsoup.parse(index.toFile(), "UTF-8");
+    tocItems(doc);
+    languageTab(doc);
+    clipboard(doc);
+    Document.OutputSettings settings = new Document.OutputSettings();
+    settings.prettyPrint(false);
+    settings.indentAmount(0);
+    settings.outline(false);
+    return doc.outputSettings(settings).toString();
+  }
+
+  private static void languageTab(Document doc) {
+    for (Element primary : doc.select(".listingblock.primary")) {
+      Element secondary = primary.nextElementSibling();
+      String secondaryTitle = secondary.selectFirst(".title").text().trim();
+      Element primaryContent = primary.selectFirst(".content");
+      Element secondaryContent = secondary.selectFirst(".content");
+      secondary.remove();
+      secondaryContent.remove();
+
+      Element title = primary.selectFirst(".title");
+
+      Element tabs = doc.createElement("div").attr("class", "switch");
+      Element tab1 = tabs.appendElement("div");
+      tab1.attr("class", "switch--item selected");
+      if (secondaryTitle.equalsIgnoreCase("Kotlin")) {
+        tab1.text("Java");
+      } else {
+        tab1.text(title.text());
+      }
+
+      if (title.text().trim().equalsIgnoreCase(tab1.text().trim())) {
+        title.remove();
+      }
+
+      Element tab2 = tabs.appendElement("div");
+      tab2.attr("class", "switch--item");
+      tab2.text(secondaryTitle);
+      tabs.appendTo(primary);
+      primaryContent.appendTo(primary);
+      secondaryContent.appendTo(primary);
+      secondaryContent.addClass("hidden");
+    }
+  }
+
+  private static void tocItems(Document doc) {
+    int l = 3;
+    while (l > 1) {
+      Elements items = doc.body().select(".sectlevel" + l);
+      for (Element it : items) {
+        String parent = it.parent().select("a").first().attr("href").replace("#", "");
+        for (Element e : it.select("li>a")) {
+          String key = e.attr("href").replace("#", "");
+          String newkey = Stream.of((parent + "-" + key).split("-"))
+              .distinct()
+              .collect(Collectors.joining("-"));
+          Element title = doc.select("#" + key).first();
+
+          title.select("a").forEach(a -> a.attr("href", "#" + newkey));
+          title.attr("id", newkey);
+          e.attr("href", "#" + newkey);
+        }
+
+      }
+      l -= 1;
+    }
+    Document.OutputSettings settings = doc.outputSettings();
+    settings.indentAmount(2);
+    settings.prettyPrint(true);
+  }
+
+  private static void clipboard(Document doc) {
+    doc.select("code").removeAttr("data-lang");
+    for (Element pre : doc.select("pre.highlight")) {
+      Element button = pre.appendElement("button");
+      button.addClass("clipboard");
+      button.text("Copy");
+      if (pre.childNodeSize() == 1 && (pre.childNode(0) instanceof TextNode)) {
+        Element div = pre.appendElement("div");
+        div.html(pre.html());
+        pre.html("");
+      }
     }
   }
 
