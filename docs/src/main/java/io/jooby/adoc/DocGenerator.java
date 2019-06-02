6e09311c9494fc67e5466561a53336d90600dd53
Edgar Espina <espina.edgar@gmail.com>
2019-06-02 19:11:18 -0300

    shorter license header

--- docs/src/main/java/io/jooby/adoc/DocGenerator.java
+++ docs/src/main/java/io/jooby/adoc/DocGenerator.java
@@ -1,17 +1,7 @@
 /**
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- *
- *    Copyright 2014 Edgar Espina
+ * Jooby https://jooby.io
+ * Apache License Version 2.0 https://jooby.io/LICENSE.txt
+ * Copyright 2014 Edgar Espina
  */
 package io.jooby.adoc;
 
@@ -35,6 +25,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.UUID;
@@ -82,6 +73,10 @@ public class DocGenerator {
         .forEach(Sneaky.throwingConsumer(it -> {
           Files.write(it, document(it).getBytes(StandardCharsets.UTF_8));
         }));
+
+    // LICENSE
+    Files.copy(basedir.getParent().resolve("LICENSE"),outdir.resolve("LICENSE.txt"),
+        StandardCopyOption.REPLACE_EXISTING);
 
     if (publish) {
       Path website = basedir.resolve("target")// Paths.get(System.getProperty("java.io.tmpdir"))
--- jooby/src/main/java/io/jooby/internal/handler/reactive/RxSubscriber.java
+++ jooby/src/main/java/io/jooby/internal/handler/reactive/RxSubscriber.java
@@ -1,16 +1,6 @@
 /**
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * Jooby https://jooby.io
+ * Apache License Version 2.0 https://jooby.io/LICENSE.txt
  * Copyright 2014 Edgar Espina
  */
 package io.jooby.internal.handler.reactive;
