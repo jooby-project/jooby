443b715102e6350ae529e124f36f43c6e9b663fc
Edgar Espina <espina.edgar@gmail.com>
2019-10-02 09:51:32 -0300

    Set maven parallel build for travis and local build

--- etc/travis.sh
+++ etc/travis.sh
@@ -2,5 +2,5 @@
 
 DIR=$(cd "$(dirname "$0")"; pwd)
 
-sh $DIR/maven.sh clean install -DskipTests=true
-sh $DIR/maven.sh checkstyle:checkstyle -P checkstyle package
+sh $DIR/maven.sh -T C1 clean install -DskipTests=true
+sh $DIR/maven.sh -T C1 checkstyle:checkstyle -P checkstyle package
