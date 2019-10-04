c2a1e161d7dbb6f0eb86471923c63600c875f7ba
Edgar Espina <espina.edgar@gmail.com>
2019-10-03 21:15:51 -0300

    Travis: custom install phase

---
+++ etc/travis-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+DIR=$(cd "$(dirname "$0")"; pwd)
+
+sh $DIR/maven.sh clean install -q -DskipTests=true
--- etc/travis.sh
+++ etc/travis.sh
@@ -2,5 +2,4 @@
 
 DIR=$(cd "$(dirname "$0")"; pwd)
 
-sh $DIR/maven.sh clean install -q -DskipTests=true
 sh $DIR/maven.sh -Dlogback.configurationFile=$DIR/logback-travis.xml -q checkstyle:checkstyle -P checkstyle package
