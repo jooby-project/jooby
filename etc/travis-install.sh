38873258a95c8b7094f4eb563f0215a06350adad
Edgar Espina <espina.edgar@gmail.com>
2020-01-15 20:55:25 -0300

    CI env; force to use maven central

--- etc/travis-install.sh
+++ etc/travis-install.sh
@@ -2,4 +2,4 @@
 
 DIR=$(cd "$(dirname "$0")"; pwd)
 
-sh $DIR/maven.sh clean install -q -DskipTests -s $DIR/travis-settings.xml
+sh $DIR/maven.sh clean install -q -DskipTests -s $DIR/central-settings.xml
--- etc/travis.sh
+++ etc/travis.sh
@@ -2,4 +2,4 @@
 
 DIR=$(cd "$(dirname "$0")"; pwd)
 
-sh $DIR/maven.sh clean checkstyle:checkstyle -P checkstyle package -s $DIR/travis-settings.xml
+sh $DIR/maven.sh clean checkstyle:checkstyle -P checkstyle package -s $DIR/central-settings.xml
