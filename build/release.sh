50c22124875f16f3fe71a8ad8837f7e4b489009f
Edgar Espina <espina.edgar@gmail.com>
2020-07-28 14:55:04 -0300

    v1.6.8

--- build/release.sh
+++ build/release.sh
@@ -2,9 +2,9 @@
 
 mvn -pl '!modules/coverage-report' clean deploy -P sonatype-oss-release
 
-cd modules/jooby-bom
+# cd modules/jooby-bom
 
-groovy bom.groovy > pom.xml
+# groovy bom.groovy > pom.xml
 
 # mvn -Dalpn-boot-version=$ALPN_VERSION clean deploy -P sonatype-oss-release
-mvn clean deploy -P sonatype-oss-release
+# mvn clean deploy -P sonatype-oss-release
