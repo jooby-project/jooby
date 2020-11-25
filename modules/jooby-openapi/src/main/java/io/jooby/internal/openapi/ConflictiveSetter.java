e33b2450d9deb7a66507a2afe540413e6b9f80ae
Edgar Espina <espina.edgar@gmail.com>
2020-11-25 17:35:21 -0300

    v2.9.4

--- modules/jooby-openapi/src/main/java/io/jooby/internal/openapi/ConflictiveSetter.java
+++ modules/jooby-openapi/src/main/java/io/jooby/internal/openapi/ConflictiveSetter.java
@@ -1,3 +1,8 @@
+/**
+ * Jooby https://jooby.io
+ * Apache License Version 2.0 https://jooby.io/LICENSE.txt
+ * Copyright 2014 Edgar Espina
+ */
 package io.jooby.internal.openapi;
 
 import java.util.List;
