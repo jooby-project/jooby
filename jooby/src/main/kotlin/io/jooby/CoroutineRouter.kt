6f7b519c61199fbb9e836b838d8c067d1ad92c73
Anton Keks <anton@codeborne.com>
2021-04-28 11:00:40 +0300

    Extract common launching logic for both MVC-style and lambda-style routes

--- jooby/src/main/kotlin/io/jooby/CoroutineRouter.kt
+++ jooby/src/main/kotlin/io/jooby/CoroutineRouter.kt
@@ -5,16 +5,10 @@
  */
 package io.jooby
 
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.CoroutineStart
-import kotlinx.coroutines.asCoroutineDispatcher
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
-internal class RouterCoroutineScope(coroutineContext: CoroutineContext) : CoroutineScope {
-  override val coroutineContext = coroutineContext
-}
+internal class RouterCoroutineScope(override val coroutineContext: CoroutineContext) : CoroutineScope
 
 class CoroutineRouter(val coroutineStart: CoroutineStart, val router: Router) {
 
@@ -64,10 +58,7 @@ class CoroutineRouter(val coroutineStart: CoroutineStart, val router: Router) {
 
   fun route(method: String, pattern: String, handler: suspend HandlerContext.() -> Any): Route {
     return router.route(method, pattern) { ctx ->
-      val xhandler = CoroutineExceptionHandler { _, x ->
-        ctx.sendError(x)
-      }
-      coroutineScope.launch(xhandler, coroutineStart) {
+      launch(ctx) {
         val result = handler(HandlerContext(ctx))
         if (result != ctx) {
           ctx.render(result)
@@ -75,5 +66,10 @@ class CoroutineRouter(val coroutineStart: CoroutineStart, val router: Router) {
       }
     }.setHandle(handler)
      .attribute("coroutine", true)
+  }
+
+  internal fun launch(ctx: Context, block: suspend CoroutineScope.() -> Unit) {
+    val exceptionHandler = CoroutineExceptionHandler { _, x -> ctx.sendError(x) }
+    coroutineScope.launch(exceptionHandler, coroutineStart, block)
   }
 }
