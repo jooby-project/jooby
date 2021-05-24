124d947ca822544b915c48aa64ea034b0c5a8162
Edgar Espina <espina.edgar@gmail.com>
2021-05-24 20:43:21 -0300

    jooby-gradle-plugin: Update plugint to work with 6.8 or higher
    - jooby-gradle-plugin isn't a maven project anymore
    - code was updated to work  on 6.8 or higher
    - an error is displayed when running old version
    - update plugins to use `mainClass`
    - deprecate `mainClassName`
    - update cli-generator to use latest gradle 7.x
    
    Fixes #2326 #2351

--- modules/jooby-gradle-plugin/src/main/java/io/jooby/gradle/BaseTask.java
+++ modules/jooby-gradle-plugin/src/main/java/io/jooby/gradle/BaseTask.java
@@ -5,13 +5,6 @@
  */
 package io.jooby.gradle;
 
-import org.gradle.api.DefaultTask;
-import org.gradle.api.Project;
-import org.gradle.api.artifacts.Configuration;
-import org.gradle.api.plugins.JavaPluginConvention;
-import org.gradle.api.tasks.SourceSet;
-
-import javax.annotation.Nonnull;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -22,10 +15,22 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.annotation.Nonnull;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.plugins.JavaApplication;
+import org.gradle.api.plugins.JavaPluginConvention;
+import org.gradle.api.tasks.Internal;
+import org.gradle.api.tasks.SourceSet;
 
 /**
  * Base class which provides common utility method to more specific plugins: like classpath
@@ -37,13 +42,14 @@ import java.util.stream.Collectors;
  */
 public class BaseTask extends DefaultTask {
 
-  protected static final String APP_CLASS = "mainClassName";
+  protected static final String APP_CLASS_NAME = "mainClassName";
 
   /**
    * Available projects.
    *
    * @return Available projects.
    */
+  @Internal
   public @Nonnull List<Project> getProjects() {
     return Collections.singletonList(getProject());
   }
@@ -56,11 +62,22 @@ public class BaseTask extends DefaultTask {
    */
   protected @Nonnull String computeMainClassName(@Nonnull List<Project> projects) {
     return projects.stream()
-        .filter(it -> it.getProperties().containsKey(APP_CLASS))
-        .map(it -> it.getProperties().get(APP_CLASS).toString())
+        .map(it -> {
+          // Old way:
+          String mainClassName = Optional.ofNullable(it.getProperties().get(APP_CLASS_NAME))
+              .map(Objects::toString)
+              .orElseGet(() ->
+                // New way:
+                Optional.ofNullable(it.getConvention().findByType(JavaApplication.class))
+                    .map(plugin -> plugin.getMainClass().getOrNull())
+                    .orElse(null)
+              );
+          return mainClassName;
+        })
+        .filter(Objects::nonNull)
         .findFirst()
         .orElseThrow(() -> new IllegalArgumentException(
-            "Application class not found. Did you forget to set `" + APP_CLASS + "`?"));
+            "Application class not found. Did you forget to set `" + APP_CLASS_NAME + "`?"));
   }
 
   /**
@@ -135,7 +152,8 @@ public class BaseTask extends DefaultTask {
    * @param sourceSet Source set.
    * @return Source directories.
    */
-  protected @Nonnull Set<Path> sourceDirectories(@Nonnull Project project, @Nonnull SourceSet sourceSet) {
+  protected @Nonnull Set<Path> sourceDirectories(@Nonnull Project project,
+      @Nonnull SourceSet sourceSet) {
     Path eclipse = project.getProjectDir().toPath().resolve(".classpath");
     if (Files.exists(eclipse)) {
       // let eclipse to do the incremental compilation
