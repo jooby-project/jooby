b0145ad7c13b2a882321088b5386757c25470fa2
Filipe Roque <froque@premium-minds.com>
2021-06-14 15:04:11 +0100

    Adds support for JSON-B (JSR 367) using yasson as the implementation.
    There was a previous similar addition for jooby 1 that was not included in
    jooby 2:
    https://github.com/jooby-project/jooby/pull/1248
    https://jooby.io/v1/doc/yasson/

---
+++ modules/jooby-yasson/src/main/java/io/jooby/json/YassonModule.java
@@ -0,0 +1,113 @@
+package io.jooby.json;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import io.jooby.Body;
+import io.jooby.Context;
+import io.jooby.Extension;
+import io.jooby.Jooby;
+import io.jooby.MediaType;
+import io.jooby.MessageDecoder;
+import io.jooby.MessageEncoder;
+import io.jooby.ServiceRegistry;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+
+/**
+ * JSON module using JSON-B: https://github.com/eclipse-ee4j/jsonb-api.
+ *
+ * Usage:
+ *
+ * <pre>{@code
+ * {
+ *
+ *   install(new YassonModule());
+ *
+ *   get("/", ctx -> {
+ *     MyObject myObject = ...;
+ *     // send json
+ *     return myObject;
+ *   });
+ *
+ *   post("/", ctx -> {
+ *     // read json
+ *     MyObject myObject = ctx.body(MyObject.class);
+ *     // send json
+ *     return myObject;
+ *   });
+ * }
+ * }</pre>
+ *
+ * For body decoding the client must specify the <code>Content-Type</code> header set to
+ * <code>application/json</code>.
+ *
+ * You can retrieve the {@link Jsonb} object via require call:
+ *
+ * <pre>{@code
+ * {
+ *
+ *   Jsonb jsonb = require(Jsonb.class);
+ *
+ * }
+ * }</pre>
+ *
+ * Complete documentation is available at: https://jooby.io/modules/jsonb.
+ *
+ */
+public class YassonModule implements Extension, MessageDecoder, MessageEncoder {
+
+  private final Jsonb jsonb;
+
+  /**
+   * Creates a new Jsonb module.
+   */
+  public YassonModule() {
+    jsonb = JsonbBuilder.create();
+  }
+
+  /**
+   * Creates a new module and use a Jsonb instance.
+   *
+   * @param jsonb Jsonb to use.
+   */
+  public YassonModule(@Nonnull final Jsonb jsonb) {
+    this.jsonb = jsonb;
+  }
+
+  @Override
+  public void install(@Nonnull final Jooby application) throws Exception {
+    application.decoder(MediaType.json, this);
+    application.encoder(MediaType.json, this);
+
+    ServiceRegistry services = application.getServices();
+    services.put(Jsonb.class, jsonb);
+  }
+
+  @Nonnull
+  @Override
+  public Object decode(
+      @Nonnull final Context ctx,
+      @Nonnull final Type type) throws IOException {
+
+    Body body = ctx.body();
+    try (InputStream stream = body.stream()) {
+      return jsonb.fromJson(new InputStreamReader(stream), type);
+    }
+  }
+
+  @Nullable
+  @Override
+  public byte[] encode(
+      @Nonnull final Context ctx,
+      @Nonnull final Object value) {
+    ctx.setDefaultResponseType(MediaType.json);
+    return jsonb.toJson(value).getBytes(UTF_8);
+  }
+}
