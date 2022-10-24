82c4281fe3736d7ab82d5edd15f71381e574a8fa
Edgar Espina <espina.edgar@gmail.com>
2022-10-24 18:09:35 -0300

    Preflight requests fail with 415 when MVC Controller method has @Consumes Annotation fix #2649

---
+++ tests/src/test/java/io/jooby/Issue2649.java
@@ -0,0 +1,38 @@
+package io.jooby;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.jooby.junit.ServerTest;
+import io.jooby.junit.ServerTestRunner;
+
+public class Issue2649 {
+
+  @ServerTest
+  public void shouldDoPreflightWithout415(ServerTestRunner runner) {
+    runner.define(app -> {
+      app.decorator(new CorsHandler(new Cors()));
+
+      app.post("/consumes", Context::getRequestPath)
+          .consumes(MediaType.json);
+      app.post("/produces", Context::getRequestPath)
+          .produces(MediaType.json);
+
+    }).ready(client -> {
+      // OPTIONS (Pre-flight), checking GET Method => OK and Access Control Headers Present
+      client
+          .header("Access-Control-Request-Method", "POST")
+          .options("/consumes", rsp -> {
+            assertEquals("/consumes", rsp.body().string());
+            assertEquals(200, rsp.code());
+          });
+
+      client
+          .header("Access-Control-Request-Method", "POST")
+          .options("/produces", rsp -> {
+            assertEquals("/produces", rsp.body().string());
+            assertEquals(200, rsp.code());
+          });
+    });
+  }
+
+}
