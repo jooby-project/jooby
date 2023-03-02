jooby-project/jooby

commit 9640984a949890948f15c472e47df3086005b890

encoder: should consider route produce types, fixes #2613

jooby/src/main/java/io/jooby/internal/HttpMessageEncoder.java
@@ -100,7 +100,11 @@ public class HttpMessageEncoder implements MessageEncoder {
     }
     if (encoders != null) {
       // Content negotiation, find best:
-      MediaType type = ctx.accept(new ArrayList<>(encoders.keySet()));
+      List<MediaType> produces = ctx.getRoute().getProduces();
+      if (produces.isEmpty()) {
+        produces = new ArrayList<>(encoders.keySet());
+      }
+      MediaType type = ctx.accept(produces);
       MessageEncoder encoder = encoders.getOrDefault(type, MessageEncoder.TO_STRING);
       return encoder.encode(ctx, value);
     } else {

jooby/src/main/java/io/jooby/DefaultContext.java
@@ -23,6 +23,7 @@ import java.time.Instant;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -205,7 +206,7 @@ public interface DefaultContext extends Context {
   }
 
   @Override default boolean accept(@Nonnull MediaType contentType) {
-    return accept(singletonList(contentType)) == contentType;
+    return accept(singletonList(contentType)).equals(contentType);
   }
 
   @Override default MediaType accept(@Nonnull List<MediaType> produceTypes) {

tests/src/test/java/io/jooby/i2613/Issue2613.java
@@ -1,0 +1,50 @@
+package io.jooby.i2613;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.nio.charset.StandardCharsets;
+
+import org.json.JSONObject;
+
+import com.google.common.collect.ImmutableMap;
+import io.jooby.Context;
+import io.jooby.MediaType;
+import io.jooby.MessageEncoder;
+import io.jooby.json.JacksonModule;
+import io.jooby.junit.ServerTest;
+import io.jooby.junit.ServerTestRunner;
+
+public class Issue2613 {
+
+  public static class ThemeResult {
+    public String html() {
+      return "<html></html>";
+    }
+  }
+
+  public static class ThemeResultEncoder implements MessageEncoder {
+
+    @Override public byte[] encode(Context ctx, Object value) throws Exception {
+      if (value instanceof ThemeResult) {
+        ctx.setDefaultResponseType(MediaType.html);
+        return ((ThemeResult) value).html().getBytes(StandardCharsets.UTF_8);
+      }
+      return null;
+    }
+
+  }
+
+  @ServerTest public void shouldConsiderRouteProduces(ServerTestRunner runner) {
+    runner.define(app -> {
+      app.encoder(MediaType.html, new ThemeResultEncoder());
+      app.install(new JacksonModule());
+
+      app.get("/2613/json", ctx -> ImmutableMap.of("foo", "bar"))
+          .produces(MediaType.json);
+    }).ready(http -> {
+      http.get("/2613/json", rsp -> {
+        assertEquals("{\"foo\":\"bar\"}", rsp.body().string());
+      });
+    });
+  }
+}

commit 7d85f96a89131d07c415ac0c2b0c7c35031d0a5b

encoder: should consider all produce types before default TO_STRING
fixes #2804

Fixes regression introduced by 9640984a949890948f15c472e47df3086005b890 to
fix issue #2613 internal.HttpMessageEncoder should consider the Route
produces for determining MediaType

jooby/src/main/java/io/jooby/internal/HttpMessageEncoder.java
@@ -12,9 +12,13 @@ import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import javax.annotation.Nonnull;
 
@@ -104,9 +108,18 @@ public class HttpMessageEncoder implements MessageEncoder {
       if (produces.isEmpty()) {
         produces = new ArrayList<>(encoders.keySet());
       }
-      MediaType type = ctx.accept(produces);
-      MessageEncoder encoder = encoders.getOrDefault(type, MessageEncoder.TO_STRING);
-      return encoder.encode(ctx, value);
+
+      List<MediaType> producesSubList = new ArrayList<>(produces);
+      while (!producesSubList.isEmpty()){
+        final MediaType type = ctx.accept(producesSubList);
+        MessageEncoder encoder = encoders.get(type);
+        if (encoder != null){
+          return encoder.encode(ctx, value);
+        }
+        producesSubList = producesSubList.subList(1, producesSubList.size());
+      }
+
+      return MessageEncoder.TO_STRING.encode(ctx, value);
     } else {
       return MessageEncoder.TO_STRING.encode(ctx, value);
     }

tests/src/test/java/io/jooby/i2804/Issue2804.java
@@ -1,0 +1,63 @@
+package io.jooby.i2804;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import io.jooby.MediaType;
+import io.jooby.StatusCode;
+import io.jooby.json.JacksonModule;
+import io.jooby.junit.ServerTest;
+import io.jooby.junit.ServerTestRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Issue2804 {
+
+    public static class Error {
+        public String message;
+    }
+
+    public static class Errors {
+        public List<Error> errors = new ArrayList<>();
+    }
+
+    @ServerTest
+    public void shouldConsiderRouteProduces(ServerTestRunner runner) {
+        runner.define(app -> {
+
+            app.install(new JacksonModule());
+            app.get("/pdf", ctx -> {
+
+                if (ctx.header("Authorization").isMissing()){
+                    ctx.setResponseCode(StatusCode.UNAUTHORIZED).setResponseType(MediaType.json);
+                    final Errors errors = new Errors();
+                    final Error error = new Error();
+                    error.message = "No Authorization provided";
+                    errors.errors.add(error);
+                    return errors;
+                }
+
+                ctx.setResponseCode(StatusCode.OK).setResponseType(MediaType.byFileExtension("pdf"));
+                byte[] dummyData = new byte[1024];
+                Arrays.fill(dummyData, (byte) 0x0a);
+                return new ByteArrayInputStream(dummyData);
+
+            }).produces(MediaType.byFileExtension("pdf"), MediaType.json);
+
+        }).ready(http -> {
+            http.get("/pdf", rsp -> {
+                assertEquals(StatusCode.UNAUTHORIZED_CODE, rsp.code());
+                assertEquals(MediaType.json.toContentTypeHeader(StandardCharsets.UTF_8).toLowerCase(), rsp.header("Content-Type").toLowerCase());
+                assertEquals("{\"errors\":[{\"message\":\"No Authorization provided\"}]}", rsp.body().string());
+            });
+            http.get("/pdf").prepare(req -> req.addHeader("Authorization", "foo")).execute(rsp -> {
+                assertEquals(StatusCode.OK_CODE, rsp.code());
+                assertEquals(MediaType.byFileExtension("pdf").getValue(), rsp.header("Content-Type"));
+                assertEquals(1024, rsp.body().bytes().length);
+            });
+        });
+    }
+}