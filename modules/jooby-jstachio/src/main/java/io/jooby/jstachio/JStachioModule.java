b484ccea7a5559f633db307612fb19d656a40987
Adam Gent <adam.gent@snaphop.com>
2023-04-05 11:49:36 -0400

    Missing jakarta module ref

--- modules/jooby-jstachio/src/main/java/io/jooby/jstachio/JStachioModule.java
+++ modules/jooby-jstachio/src/main/java/io/jooby/jstachio/JStachioModule.java
@@ -1,7 +1,7 @@
 /*
  * Jooby https://jooby.io
  * Apache License Version 2.0 https://jooby.io/LICENSE.txt
- * Copyright 2023 Edgar Espina
+ * Copyright 2014 Edgar Espina
  */
 package io.jooby.jstachio;
 
@@ -21,17 +21,15 @@ import io.jstach.jstachio.spi.JStachioExtension;
 import io.jstach.jstachio.spi.JStachioFactory;
 
 /**
- * Allows Jooby to render JStachio models.
- * If a JStachio is not set on this module before calling
- * install then a ServiceLoader based JStachio will be used.
- * See <a href="https://jstach.io/doc/jstachio/1.x/apidocs">JStachio documentation</a>.
- * <p>
- * For development mode to allow hot reloading add (currently experimental) 
- * the dependency of <code>io.jstach:jstachio-jmustache:VERSION</code> (Maven GAV).
- * <strong>Development mode is enabled by default if the extension is found.
- * to disable set <code>JSTACHIO_JMUSTACHE_DISABLE</code> to <code>true</code>!</strong>
- * 
- * 
+ * Allows Jooby to render JStachio models. If a JStachio is not set on this module before calling
+ * install then a ServiceLoader based JStachio will be used. See <a
+ * href="https://jstach.io/doc/jstachio/1.x/apidocs">JStachio documentation</a>.
+ *
+ * <p>For development mode to allow hot reloading add (currently experimental) the dependency of
+ * <code>io.jstach:jstachio-jmustache:VERSION</code> (Maven GAV). <strong>Development mode is
+ * enabled by default if the extension is found. to disable set <code>JSTACHIO_JMUSTACHE_DISABLE
+ * </code> to <code>true</code>!</strong>
+ *
  * @author agentgt
  * @since 3.0.0
  */
@@ -40,9 +38,10 @@ public class JStachioModule implements Extension {
   private @Nullable JStachio jstachio;
   private Charset charset = StandardCharsets.UTF_8;
   private int bufferSize = 8 * 1024;
-  
+
   /**
    * Sets the jstachio to use instead of the default.
+   *
    * @param jstachio the jstachio instance to be used instead of the default.
    * @return this
    */
@@ -50,9 +49,10 @@ public class JStachioModule implements Extension {
     this.jstachio = jstachio;
     return this;
   }
-  
+
   /**
    * Sets the character set for rendering. Default is <code>UTF8</code>.
+   *
    * @param charset never null.
    * @return this
    */
@@ -60,9 +60,10 @@ public class JStachioModule implements Extension {
     this.charset = Objects.requireNonNull(charset);
     return this;
   }
-  
+
   /**
    * Sets the initial buffer size to be used for rendering. Default is <code>8 * 1024</code>.
+   *
    * @param bufferSize an integer 0 or greater.
    * @return this
    * @throws IllegalArgumentException if the bufferSize is less than <code>0</code>.
@@ -74,10 +75,9 @@ public class JStachioModule implements Extension {
     this.bufferSize = bufferSize;
     return this;
   }
-  
+
   /**
-   * Installs JStachio into Jooby and provides the JStachio service to
-   * the {@link ServiceRegistry}.
+   * Installs JStachio into Jooby and provides the JStachio service to the {@link ServiceRegistry}.
    * {@inheritDoc}
    */
   @Override
@@ -87,31 +87,32 @@ public class JStachioModule implements Extension {
     if (j == null) {
       j = services.getOrNull(JStachio.class);
       if (j == null) {
-        final JStachio joobyJStachio = JStachioFactory.builder()
-            .add(extensions(application))
-            .add(new JoobyJStachioConfig(application.getEnvironment()))
-            .build();
+        final JStachio joobyJStachio =
+            JStachioFactory.builder()
+                .add(extensions(application))
+                .add(new JoobyJStachioConfig(application.getEnvironment()))
+                .build();
         j = joobyJStachio;
         this.jstachio = j;
-        services.put(JStachio.class, () -> joobyJStachio);
+        services.put(JStachio.class, joobyJStachio);
       }
-    }
-    else {
-      services.put(JStachio.class, () -> this.jstachio);
+    } else {
+      services.put(JStachio.class, this.jstachio);
     }
     JStachioMessageEncoder encoder = new JStachioMessageEncoder(j, this.charset, this.bufferSize);
     JStachioResultHandler handler = new JStachioResultHandler(encoder);
     application.encoder(MediaType.html, encoder);
     application.resultHandler(handler);
   }
-  
+
   /**
    * Default implementation uses the ServiceLoader to load extensions.
+   *
    * @param application used to get the environments classloader.
    * @return found extensions.
    */
   protected Iterable<JStachioExtension> extensions(Jooby application) {
-    return ServiceLoader.load(JStachioExtension.class, application.getEnvironment().getClassLoader());
+    return ServiceLoader.load(
+        JStachioExtension.class, application.getEnvironment().getClassLoader());
   }
-  
 }
