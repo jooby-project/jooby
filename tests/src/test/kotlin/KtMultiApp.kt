dae444521d6868dce967ffa55ee2b2575f4eec79
Edgar Espina <espina.edgar@gmail.com>
2025-04-28 10:47:45 -0300

    feature: support multiple application on single server - kotlin idiom + doc - fix #3645

---
+++ tests/src/test/kotlin/KtMultiApp.kt
@@ -0,0 +1,18 @@
+/*
+ * Jooby https://jooby.io
+ * Apache License Version 2.0 https://jooby.io/LICENSE.txt
+ * Copyright 2014 Edgar Espina
+ */
+import examples.multiapp.BarApp
+import examples.multiapp.FooApp
+import io.jooby.ExecutionMode
+import io.jooby.kt.runApp
+import io.jooby.netty.NettyServer
+
+fun main(args: Array<String>) {
+  runApp(args, NettyServer(), ExecutionMode.DEFAULT, ::BarApp, ::FooApp)
+
+  runApp(args, NettyServer(), ::BarApp, ::FooApp)
+
+  runApp(args, ::BarApp, ::FooApp)
+}
