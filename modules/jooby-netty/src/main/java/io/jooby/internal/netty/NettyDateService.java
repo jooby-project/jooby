5f42f209e636954372a33665dc2890bf008411ae
Edgar Espina <espina.edgar@gmail.com>
2026-03-10 07:27:56 -0300

    jooby-netty: possible leak in scheduled tasks under HTTP traffic
    - only affect 4.x
    - fix #3871

--- modules/jooby-netty/src/main/java/io/jooby/internal/netty/NettyDateService.java
+++ modules/jooby-netty/src/main/java/io/jooby/internal/netty/NettyDateService.java
@@ -8,18 +8,25 @@ package io.jooby.internal.netty;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+
+import io.netty.util.concurrent.DefaultThreadFactory;
 
 public class NettyDateService implements Runnable {
   private static final int DATE_INTERVAL = 1000;
-  private CharSequence date;
+  private final ScheduledExecutorService scheduler;
+  // Make volatile for thread visibility across EventLoop threads
+  private volatile CharSequence date;
+  private final ScheduledFuture<?> future;
 
-  public NettyDateService(ScheduledExecutorService scheduler) {
+  public NettyDateService() {
+    this.scheduler =
+        Executors.newSingleThreadScheduledExecutor(new DefaultThreadFactory("http-date-keeper"));
     this.date =
         new NettyString(
             DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneOffset.UTC)));
-    scheduler.scheduleAtFixedRate(this, DATE_INTERVAL, DATE_INTERVAL, TimeUnit.MILLISECONDS);
+    this.future =
+        scheduler.scheduleAtFixedRate(this, DATE_INTERVAL, DATE_INTERVAL, TimeUnit.MILLISECONDS);
   }
 
   public CharSequence date() {
@@ -31,5 +38,18 @@ public class NettyDateService implements Runnable {
     this.date =
         new NettyString(
             DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneOffset.UTC)));
+  }
+
+  // Allow the server to gracefully terminate the background task
+  public void stop() {
+    try {
+      if (this.future != null) {
+        this.future.cancel(false);
+      }
+    } catch (Exception ignored) {
+      if (scheduler != null) {
+        scheduler.shutdown();
+      }
+    }
   }
 }
